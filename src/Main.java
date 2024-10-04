1846f03f212fd267ca83beaf34821058adbab0ea
PilotVelosipeda <vsotnikov35@mail.ru>
2024-10-04 21:13:33 +0300

    ApplicationForCommunication

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int foundHeight = 173;
+        int foundAge = 21;
+        double foundWeight = 55.1;
+        String foundHobby = "Бизнес-литература";
+        String foundMainClass = "Учеба";
+        boolean havePartner = false;
+
+        while (!havePartner) {
+            //Ищем партнёра
+            System.out.println("Введите свой рост в см: ");
+            int height = new Scanner(System.in).nextInt();
+
+            System.out.println("Введите свой возраст: ");
+            int age = new Scanner(System.in).nextInt();
+
+            System.out.println("Введите свой вес: ");
+            double weight = new Scanner(System.in).nextDouble();
+
+            System.out.println("Назовите своё хобби: ");
+            String hobby = new Scanner(System.in).nextLine();
+
+            System.out.println("Расскажите, чем занимаетесь в основное время: ");
+            String mainClass = new Scanner(System.in).nextLine();
+
+            havePartner = (
+                    (height >= foundHeight - 23 || height <= foundHeight + 3) &&
+                    (age >= foundAge - 3 || age <= foundAge + 1) &&
+                    (weight >= foundWeight - 15.9 || weight <= foundWeight + 4.9) &&
+                            (hobby.compareToIgnoreCase(foundHobby) == 0 || hobby.contains(foundHobby)) &&
+                    (mainClass.compareToIgnoreCase(foundMainClass) == 0 || hobby.contains(mainClass))
+                    ) ? true : false;
+
+            if (!havePartner) {
+                System.out.println("Попроюуем поискать еще, вы не подошли по указанным параметрам");
+            }
+
+        }
+
+        System.out.println("Половинка нашлась :) ");
+    }
+}
